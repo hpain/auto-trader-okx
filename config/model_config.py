61b31e67f29e088f19168ba3de17aa3dc93e12c4
hpain <kunhangg@gmail.com>
2025-08-24 22:27:33 +0800

    Update model_config.py

--- config/model_config.py
+++ config/model_config.py
@@ -1,16 +1,19 @@
 # config/model_config.py
-
 MODEL_PARAMS = {
     "logreg": {
-        # 默认参数
         "solver": "lbfgs",
         "max_iter": 1000,
         "use_scaler": True,
-
-        # Optuna 搜索空间
         "C_range": [1e-3, 1e2]
     },
-
-    # 未来可以加其它模型的默认配置
-    # "lgbm": {...}
+    "rf": {
+        "n_estimators_range": [100, 500],
+        "max_depth_range": [3, 12],
+        "min_samples_leaf_range": [1, 8]
+    },
+    "lgb": {
+        "num_leaves_range": [15, 63],
+        "learning_rate_range": [0.01, 0.2]
+    }
 }
+
