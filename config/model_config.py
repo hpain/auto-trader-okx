98c2eb90f5ff6dd812b85ac559e910217a18bbbc
hpain <kunhangg@gmail.com>
2025-08-25 16:37:22 +0800

    Update model_config.py

--- config/model_config.py
+++ config/model_config.py
@@ -1,10 +1,12 @@
-# config/model_config.py
 MODEL_PARAMS = {
     "logreg": {
         "solver": "lbfgs",
         "max_iter": 1000,
         "use_scaler": True,
-        "C_range": [1e-3, 1e2]
+        "C_range": [2, 12],   # 原来是 [1e-3, 1e2]
+        "buy_th_range": [0.58, 0.62],
+        "sell_th_range": [0.44, 0.48],
+        "fee_range": [0.0002, 0.0010]
     },
     "rf": {
         "n_estimators_range": [100, 500],
@@ -16,4 +18,3 @@ MODEL_PARAMS = {
         "learning_rate_range": [0.01, 0.2]
     }
 }
-
