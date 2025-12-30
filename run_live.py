f18e3838f73161499ece8c43140d6d3b546432ce
hpain <kunhangg@gmail.com>
2025-12-30 22:03:29 +0800

    Debug

--- run_live.py
+++ run_live.py
@@ -23,6 +23,8 @@ async def main_loop(args):
     """
     全新的自动化交易主循环，集成了市场状态检测和策略管理。
     """
+
+    print("11111111111111111111111111111111 开始开始开始")
     # --- SAFETY CHECK: PREVENT ACCIDENTAL LIVE/PAPER TRADING ON DEV MACHINE ---
     if sys.platform == 'win32' and not args.mock and not os.environ.get('ALLOW_LOCAL_TRADING'):
         print("\n" + "!" * 80)
